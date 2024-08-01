a74d354c3fe0b8ae81d9f1f46a8bb264143b8f3d
Nicholas Ilechie <160823545+nnilechie@users.noreply.github.com>
2024-07-31 21:14:57 -0400

    update

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ app.use((req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
+//const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port`);
 });
