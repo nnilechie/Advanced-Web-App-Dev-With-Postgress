135a87f2d6b2b792a1419d39795f5342c5609b15
Nicholas Ilechie <160823545+nnilechie@users.noreply.github.com>
2024-08-05 13:25:20 -0400

    vercel update

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Middleware to serve static files from the 'public' directory
 app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // Middleware for Nav
 app.use(function(req, res, next) {
